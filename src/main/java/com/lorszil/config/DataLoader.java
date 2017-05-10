b7b4a703e1ba6d7d34ec8c2e68091895e060bedb
lorszil <lorszil@gmail.com>
2017-05-10 12:03:13 +0200

    creating admin to the page

---
+++ src/main/java/com/lorszil/config/DataLoader.java
@@ -0,0 +1,35 @@
+package com.lorszil.config;
+
+import com.lorszil.model.Profile;
+import com.lorszil.model.Roles;
+import com.lorszil.model.User;
+import com.lorszil.repository.ProfileRepository;
+import com.lorszil.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+public class DataLoader {
+
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @PostConstruct
+    public void loadAdmin() {
+        if (userService.getUserByEmail("admin@admin.com").equals(Optional.empty())) {
+            User admin = new User("admin@admin.com", "12345");
+            userService.create(admin, Roles.ADMIN);
+            Profile profile = new Profile(admin);
+            profileRepository.save(profile);
+        }
+    }
+}
