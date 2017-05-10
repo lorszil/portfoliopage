28ce1348efe6ed58e8706d3bddf42d0dbdddb8e0
lorszil <lorszil@gmail.com>
2017-05-10 12:03:44 +0200

    fixing some issue in the websec for spring

--- src/main/java/com/lorszil/config/WebSecurityConfig.java
+++ src/main/java/com/lorszil/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.lorszil.config;
 
-
+import com.lorszil.filter.AuthenticationFilter;
+import com.lorszil.model.Roles;
+import com.lorszil.service.ApiAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,22 +10,40 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/admin/**").hasAuthority(Roles.ADMIN.toString())
                 .antMatchers("/", "/blog").permitAll()
-                .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
+                .formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/admin/dashboard")
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").deleteCookies("JSESSIONID")
                 .permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                .exceptionHandling();
     }
 
     @Autowired
@@ -32,4 +52,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
     }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    ApiAuthenticationService apiAuthenticationService() {
+        return new ApiAuthenticationService();
+    }
+
+
 }
