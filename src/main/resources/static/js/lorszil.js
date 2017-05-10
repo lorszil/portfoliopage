61952eba4977bca01f8bde0a3545b6cbd47085e0
lorszil <lorszil@gmail.com>
2017-05-10 12:01:12 +0200

    init for spring

---
+++ src/main/resources/static/js/lorszil.js
@@ -0,0 +1,18 @@
+'use strict';
+
+lorszil = angular.module('lorszil', ['ngRoute'])
+
+    .config(['$httpProvider', '$locationProvider',
+        function ($httpProvider, $locationProvider) {
+            $httpProvider.defaults.withCredentials = true;
+            $httpProvider.defaults.headers['Content-Type'] = 'application/json; charset=UTF-8';
+            $httpProvider.defaults.useXDomain = true;
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+            $httpProvider.defaults.headers.post["Content-Type"] = "application/json";
+
+            $locationProvider
+                .html5Mode(true)
+                .hashPrefix('!');
+
+        }]);
+
