b8749e2f3470fa73fbc927754dc82da468eb47e3
lorszil <lorszil@gmail.com>
2017-05-10 12:01:59 +0200

    fixing some problem

--- src/main/java/com/lorszil/LorszilApplication.java
+++ src/main/java/com/lorszil/LorszilApplication.java
@@ -1,13 +1,24 @@
 package com.lorszil;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
-public class LorszilApplication {
+public class LorszilApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LorszilApplication.class);
+	}
+
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(LorszilApplication.class, args);
 	}
 }
