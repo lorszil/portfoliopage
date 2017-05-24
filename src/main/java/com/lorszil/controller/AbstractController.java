4abe498a1e5ef137d44c44a53aca95ae1609ac32
lorszil <lorszil@gmail.com>
2017-05-24 13:01:31 +0200

    fixing projectrepository

--- src/main/java/com/lorszil/controller/AbstractController.java
+++ src/main/java/com/lorszil/controller/AbstractController.java
@@ -2,27 +2,22 @@ package com.lorszil.controller;
 
 
 import com.lorszil.model.Profile;
-import com.lorszil.model.User;
-import com.lorszil.repository.ProfileRepository;
+import com.lorszil.model.Project;
+import com.lorszil.repository.ProjectRepository;
+import com.lorszil.service.ProjectService;
+import com.lorszil.service.ProjectServiceImpl;
 import com.lorszil.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
 
+@Controller
 abstract class AbstractController {
 
-    @Autowired
-    ProfileRepository profileRepository;
 
     @Autowired
-    UserService userService;
+    ProjectRepository projectRepository;
 
-    Profile getCurrentProfile(Principal principal) {
-        return profileRepository.findByUser(getCurrentUser(principal));
-    }
-
-    User getCurrentUser(Principal principal) {
-        return userService.getUserByEmail(principal.getName()).get();
-    }
 
 }
