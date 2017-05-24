4f237ece9cac4882f4b78a28e808f1cbaa890897
lorszil <lorszil@gmail.com>
2017-05-24 13:03:12 +0200

    adding annotation controller

--- src/main/java/com/lorszil/controller/BlogController.java
+++ src/main/java/com/lorszil/controller/BlogController.java
@@ -1,9 +1,11 @@
 package com.lorszil.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
+@Controller
 public class BlogController {
 
     @RequestMapping(value = "/blog", method = RequestMethod.GET)
