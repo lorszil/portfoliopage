f4e2b0cef35d24b9cdf6af860dedffe22992bfc9
lorszil <lorszil@gmail.com>
2017-05-24 13:14:09 +0200

    adding new route

--- src/main/resources/static/js/controller/routeController.js
+++ src/main/resources/static/js/controller/routeController.js
@@ -1,8 +1,13 @@
+'use strict';
 
 lorszil.config(['$routeProvider',
     function ($routeProvider) {
 
         $routeProvider
+            .when('/', {
+                templateUrl: 'index.html',
+                controller: 'menuCtrl'
+            })
             .when('/', {
                 templateUrl: 'views/main.html',
                 controller: 'mainCtrl'
@@ -11,13 +16,29 @@ lorszil.config(['$routeProvider',
                 templateUrl: 'views/blog.html',
                 controller: 'blogCtrl'
             })
-            .when('/project/:projectId', {
-                templateUrl: 'views/project.html',
-                controller: 'projectCtrl'
+            .when('/logout', {
+                templateUrl: 'index.html',
+                controller: 'logoutCtrl'
             })
+            // .when('/project/:projectId', {
+            //     templateUrl: 'views/project.html',
+            //     controller: 'projectCtrl'
+            // })
             .when('/login', {
                 templateUrl: 'views/login.html',
                 controller: 'loginCtrl'
+            })
+            .when('/projects', {
+                templateUrl: 'views/project.html',
+                controller: 'projectCtrl'
+            })
+            .when('/admin', {
+                templateUrl: 'views/admin/admin_main.html',
+                controller: 'adminMainCtrl'
+            })
+            .when('/admin/projectedit', {
+                templateUrl: 'views/admin/project_edit.html',
+                controller: 'projectEditCtrl'
             })
             .otherwise({
                 redirectTo: '/'
