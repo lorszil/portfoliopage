29e93b1b33a42fae77f0207b8a61d14db9ea6237
lorszil <lorszil@gmail.com>
2017-08-21 10:18:34 +0200

    putting a working version

--- src/main/java/com/lorszil/LorszilApplication.java
+++ src/main/java/com/lorszil/LorszilApplication.java
@@ -1,9 +1,5 @@
 package com.lorszil;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.EvictionPolicy;
-import com.hazelcast.config.ListConfig;
-import com.hazelcast.config.MapConfig;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -17,14 +13,6 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class LorszilApplication extends SpringBootServletInitializer {
 
-	@Bean
-	public Config hazelcastConfig() {
-		return new Config().setProperty("hazelcast.jmx", "true")
-				.addMapConfig(new MapConfig("spring-boot-admin-application-store").setBackupCount(1)
-						.setEvictionPolicy(EvictionPolicy.NONE))
-				.addListConfig(new ListConfig("spring-boot-admin-event-store").setBackupCount(1)
-						.setMaxSize(1000));
-	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
